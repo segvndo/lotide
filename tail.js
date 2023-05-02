b8227f6ec5525d9cd2dd9b916c21d5f683ed422d
Bea Mungcal <beamungcal@hotmail.com>
2023-05-02 12:54:19 -0400

    Returned tail end of array

---
+++ tail.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:, ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed:, ${actual} !== ${expected}`);
+  } else {
+    console.log(`Assertion Failed: undefined !== ${expected}`);
+  }
+};
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const head = function(arr) {
+  if (arr.length === 0) {
+    return undefined;
+  } else {
+    return arr[0];
+  }
+};
+
+//TEST CODE
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+const tail = function(arr) {
+  return arr.slice(1);
+};
+assertEqual(tail([5, 6, 7]), 5);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+/*
+var arr = ["item 1", "item 2", "item 3", "item 4"];
+console.log(_.tail(arr));
+*/
