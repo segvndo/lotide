5cdf2652228ed778c350d2f20472b7076508ccd9
Bea Mungcal <beamungcal@hotmail.com>
2023-05-02 21:12:16 -0400

    Used a function to match two arrays

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:, ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed:, ${actual} !== ${expected}`);
+  }
+};
+// TEST CODE
+/*assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);*/
+
+const eqArrays = function(array1, array2) {
+  if (array1.isArray !== array2.isArray) {
+    return false
+  } else {
+    for(let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i] || array1.length !== array2.length) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);
