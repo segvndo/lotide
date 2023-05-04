c24acb5e49a4b54ee60bf93b45de5b69f9f51f88
Bea Mungcal <beamungcal@hotmail.com>
2023-05-03 21:47:41 -0400

    Used function letterPositions which returned all the indices in the string where each character is found

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+const eqArrays = function(array1, array2) {
+  if (array1 === array2) {
+    return false
+  } else {
+    for(let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i] || array1.length !== array2.length) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (array1 === array2) {
+    console.log(`Assertion Passed:, ${array1} === ${array2}`);
+  } else if (array1 !== array2) {
+    console.log(`Assertion Failed:, ${array1} !== ${array2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  let i = 0;
+  for (let letter of sentence) {
+    if (letter !== " ") {
+      if (!results[letter]) {
+        results[letter] = [i];
+      } else {
+        results[letter].push(i);
+      }
+    }
+    i++;
+  }
+  return results;
+};
+console.log(letterPositions("hello"));
+
