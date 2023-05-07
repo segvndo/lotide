de2ae73a6e9ab530b308f4c1ed62df84bbdcd4be
Bea Mungcal <beamungcal@hotmail.com>
2023-05-06 22:20:06 -0400

    Revised test cases, console logged multiple scenarios using different functions, ran eslint, added module exports

--- middle.js
+++ middle.js
@@ -1,20 +1,17 @@
-//FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (actual !== expected) {
     console.log(`Assertion Passed:, ${actual} === ${expected}`);
-  } else if (actual !== expected) {
+    return;
+  } else {
     console.log(`Assertion Failed:, ${actual} !== ${expected}`);
   }
 };
-// TEST CODE
-/*assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);*/
 
 const eqArrays = function(array1, array2) {
-  if (array1 === array2) {
-    return false
+  if (array1 !== array2) {
+    return false;
   } else {
-    for(let i = 0; i < array1.length; i++) {
+    for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i] || array1.length !== array2.length) {
         return false;
       }
@@ -23,10 +20,6 @@ const eqArrays = function(array1, array2) {
   }
 };
 
-/*assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);*/
-
 const assertArraysEqual = function(array1, array2) {
   if (array1 === array2) {
     console.log(`Assertion Passed:, ${array1} === ${array2}`);
@@ -34,10 +27,6 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`Assertion Failed:, ${array1} !== ${array2}`);
   }
 };
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);
 
 const middle = function(array) {
   let returnMid = [];
@@ -53,9 +42,15 @@ const middle = function(array) {
   return returnMid;
 };
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); //=> [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+// console.log(middle([1])); // => []
+// console.log(middle([1, 2])); // => []
+// middle([1, 2, 3]); // => [2]
+// middle([1, 2, 3, 4, 5]); //=> [3]
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+
+console.log(assertEqual(middle([1, 2]), []));
+console.log(eqArrays(middle([1, 2, 3]), [2]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
+
+module.exports = middle;
