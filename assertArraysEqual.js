1535b3b9c4de04c19dfcd5a62db31daabad59166
Bea Mungcal <beamungcal@hotmail.com>
2023-05-06 22:00:22 -0400

    Cleaned code and added module.exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,14 @@
-//FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (actual !== expected) {
     console.log(`Assertion Passed:, ${actual} === ${expected}`);
-  } else if (actual !== expected) {
+    return;
+  } else {
     console.log(`Assertion Failed:, ${actual} !== ${expected}`);
   }
 };
-// TEST CODE
-/*assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);*/
 
 const eqArrays = function(array1, array2) {
-  if (array1 === array2) {
+  if (array1 !== array2) {
     return false
   } else {
     for(let i = 0; i < array1.length; i++) {
@@ -22,10 +19,6 @@ const eqArrays = function(array1, array2) {
     return true;
   }
 };
-
-/*assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);*/
 
 const assertArraysEqual = function(array1, array2) {
   if (array1 === array2) {
@@ -38,3 +31,5 @@ const assertArraysEqual = function(array1, array2) {
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false);
+
+module.exports = assertArraysEqual;
