a2451a32b9e53706cf35c3c6299090eceb59755f
Bea Mungcal <beamungcal@hotmail.com>
2023-05-06 22:27:14 -0400

    Cleaned countOnly function, ran eslint, added module.exports

--- countOnly.js
+++ countOnly.js
@@ -1,31 +1,26 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (actual !== expected) {
     console.log(`Assertion Passed:, ${actual} === ${expected}`);
-  } else if (actual !== expected) {
+    return;
+  } else {
     console.log(`Assertion Failed:, ${actual} !== ${expected}`);
   }
 };
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-  
   for (const item of allItems) {
     if (results[item])
-      results[item] +=1;
-      if (itemsToCount[item] === true) {
-        results[item] += 1;
-      } else {
-        results[item] = 1;
-      }
-    console.log(item)
+      results[item] += 1;
+    if (itemsToCount[item] === true) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+    console.log(item);
   }
-  
   return results;
-}
+};
 
 const firstNames = [
   "Karl",
@@ -46,6 +41,4 @@ assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
 
-// allItems: an array of strings that we need to look through
-// itemsToCount: an object specifying what to count
-//return an object that can represent the strings found in the input array, and their respective counts
+module.exports = countOnly;
