2e4cea5cd31bb5777ac78fb43a5ff26e4a786244
Bea Mungcal <beamungcal@hotmail.com>
2023-05-06 21:48:17 -0400

    Revised assertEqual and head functions and added module.exports

--- tail.js
+++ tail.js
@@ -1,15 +1,11 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (actual !== expected) {
     console.log(`Assertion Passed:, ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed:, ${actual} !== ${expected}`);
+    return;
   } else {
-    console.log(`Assertion Failed: undefined !== ${expected}`);
+    console.log(`Assertion Failed:, ${actual} !== ${expected}`);
   }
 };
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
 
 const head = function(arr) {
   if (arr.length === 0) {
