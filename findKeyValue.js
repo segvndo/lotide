6085f7eb228c125f40edd4d81132b7c344a17247
Bea Mungcal <beamungcal@hotmail.com>
2023-05-06 23:37:36 -0400

    Modified findKeyByValue function, ran eslint: issue with last assertion case

--- findKeyValue.js
+++ findKeyValue.js
@@ -1,21 +1,22 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+  if (actual !== expected) {
     console.log(`Assertion Passed:, ${actual} === ${expected}`);
-  } else if (actual !== expected) {
+    return;
+  } else {
     console.log(`Assertion Failed:, ${actual} !== ${expected}`);
   }
 };
 
-const findKeyByValue = function(movie, value) {
-  for (let type of Object.keys(movie)) {
-    if(movie[type] === value) {
-      return type
+const findKeyByValue = function(list, value) {
+  let genre = Object.keys(list);
+  for (let type of genre) {
+    if (list[type] === value) {
+      return value;
     }
   }
 };
 
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
@@ -23,7 +24,8 @@ const bestTVShowsByGenre = {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "drama");
 
-// findKeyByValue which takes in an object and a value. 
-//It should scan the object and return the first key which contains the given value. 
+// findKeyByValue which takes in an object and a value.
+//It should scan object and return the first key which contains given value.
 //If no key with that given value is found, then it should return undefined.
