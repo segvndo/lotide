1beb23295ae2a5aa8d00d8e43dfa4da099e172fc
Bea Mungcal <beamungcal@hotmail.com>
2023-05-07 22:54:13 -0400

    Removed test codes into headTest

--- head.js
+++ head.js
@@ -1,13 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:, ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed:, ${actual} !== ${expected}`);
-  }
-};
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   if (arr.length === 0) {
@@ -16,8 +7,5 @@ const head = function(arr) {
     return arr[0];
   }
 };
-//TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 module.exports = head;
