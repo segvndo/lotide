6ef11617a79cbb93f0e62935d2e26cbb90790883
Bea Mungcal <beamungcal@hotmail.com>
2023-05-07 22:51:18 -0400

    Removed test code to assertEqualTest

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,5 @@ const assertEqual = function(actual, expected) {
     console.log(`Assertion Failed:, ${actual} !== ${expected}`);
   }
 };
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
 
 module.exports = assertEqual;
