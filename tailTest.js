e1a1e8c8b51278a422ff98375faacce678582dd2
Bea Mungcal <beamungcal@hotmail.com>
2023-05-09 20:44:34 -0400

    Refactored tailTest file.

--- tailTest.js
+++
@@ -1,6 +0,0 @@
-const tail = require('./tail')
-const head = require('./head');
-const assertEqual = require('./assertEqual');
-
-assertEqual(tail([5, 6, 7]), 5);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
